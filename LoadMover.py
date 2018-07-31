c18071035075ed37f5cd40a1027bdfcebfcee49d
larrygardner <40435830+larrygardner@users.noreply.github.com>
2018-07-31 10:32:58 -0400

    Add files via upload

--- LoadMover.py
+++ LoadMover.py
@@ -1,76 +1,57 @@
-from uldaq import get_daq_device_inventory, DaqDevice, InterfaceType, DigitalDirection, DigitalPortIoType
+import DAQ
 import time
 import os
 
-bit_number = 7     # Control bit is at A7
-ambload = 0        
-coldload = 1
-direction = DigitalDirection.OUTPUT
-interface_type = InterfaceType.USB
+class LoadMover:
+    def __init__(self):
+        self.board_number = 0
+        self.bit_number = 7     # Control bit is at A7
+        self.ambload = 0        
+        self.coldload = 1
 
-
-devices = get_daq_device_inventory(interface_type)
-if len(devices) == 0:
-    raise Exception('Error: No DAQ devices found')
-
-print('Found', len(devices), 'DAQ device(s):')
-for i in range(len(devices)):
-    print('  ', devices[i].product_name, ' (', devices[i].unique_id, ')', sep='')
-
-# Create the DAQ device object associated with the specified descriptor index.
-daq_device = DaqDevice(devices[0])
-
-# Get the DioDevice object and verify that it is valid.
-dio_device = daq_device.get_dio_device()
-
-# Establish a connection to the DAQ device.
-descriptor = daq_device.get_descriptor()
-print('\nConnecting to', descriptor.dev_string, '- please wait...')
-daq_device.connect()
-os.system("clear")
-
-# Get the port types for the device(AUXPORT, FIRSTPORTA, ...)
-dio_info = dio_device.get_info()
-port_types = dio_info.get_port_types()
-port_to_write = port_types[0]
-
-# Configure port
-dio_device.d_config_port(port_to_write, DigitalDirection.OUTPUT)
-
-# Writes output for bit
-dio_device.d_bit_out(port_to_write, bit_number, coldload)
-
-"""
-t0 = time.time()
-for n in range(0,10,1):
-    dio_device.d_bit_out(port_to_write, bit_number, ambload)
-    time.sleep(1)
-    dio_device.d_bit_out(port_to_write, bit_number, coldload)
-    time.sleep(1)
+    def initDAQ(self):
+        # Connects DAQ
+        self.daq = DAQ.DAQ()
+        self.daq.listDevices()
+        self.daq.connect(self.board_number)
+        self.daq.configDOut()
     
-    print(n, time.time()-t0)
+    def move(self):
+        os.system("clear")
+        # Control load mover via input
+        while True:
+            move = input("Move load [up, down, or end] : ")
+            if move == "up":
+                self.daq.DOut(self.ambload, self.bit_number)
+                print("\tMoving up\n")
+            elif move == "down":
+                self.daq.DOut(self.coldload, self.bit_number)
+                print("\tMoving down\n")
+            elif move == "end":
+                print("\tEnd program\n")
+                break
+            else:
+                print("\tEnd program\n")
+                break
 
-t1 = time.time()
+    def timeMove(self, interval = 1, length = 10):
+        t0 = time.time()
+        for n in range(0, length, interval):
+            self.daq.DOut(self.ambload, self.bit_number)
+            print("\tMoving up\n")
+            time.sleep(1)
+            self.daq.DOut(self.coldload, self.bit_number)
+            print("\tMoving down\n")
+            time.sleep(1)
 
-print(t1-t0)
-"""
-# Control load mover via input
-while True:
-    move = input("Move load [up, down, or end] : ")
-    if move == "up":
-        dio_device.d_bit_out(port_to_write, bit_number, ambload)
-        print("\tMoving up\n")
-    elif move == "down":
-        dio_device.d_bit_out(port_to_write, bit_number, coldload)
-        print("\tMoving down\n")
-    elif move == "end":
-        print("\tEnd program\n")
-        break
-    else:
-        print("\tEnd program\n")
-        break
+
+    def end(self):
+        # Ends program and disconnects DAQ
+        self.daq.disconnect()
     
-daq_device.disconnect()
-daq_device.release()
-os.system("clear")
-
+if __name__ == "__main__":
+    lm = LoadMover()
+    lm.initDAQ()
+    lm.move()
+    lm.end()
+    
